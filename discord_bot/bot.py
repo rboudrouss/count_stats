9a0fa09f659e0681ff981aae128de6ee2ad8ac0e
rboudrouss <r.boudrouss@gmail.com>
2021-02-12 23:48:26 +0100

    bot now able to create a json of messages !

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,54 +1,88 @@
 import discord
-import time
-# from secret_things import TESTERS, BOT_TOKEN, CHANNEL_ID
+from time import sleep
 import json
+from typing import List, Dict
+
 
 CONFIG = json.loads(open("config.json",'r').read())
 
 class Bot(discord.Client):
-    def __init__(self, getMsg=False, getAll=False, getUsers=[], *args, **kwargs):
+    def __init__(self, getMsg=False, getAll=False, users=[], *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.getMsg = getMsg
         self.getAll = getAll
-        self.getUsers = getUsers
+        self.users_ = users
 
     async def on_ready(self):
         print('bot on !')
         if self.getMsg : await self.get_msgs(self.getAll)
-        if self.getUsers:
-            for userID in self.getUsers:
-                await self.get_user(userID)
-        await self.logout()
+        if self.users_:
+            print("calling self.get_users fonction")
+            await self.get_users(self.users_)
+        try : await self.logout()
+        except: pass
     
     async def get_msgs(self, getAll=False):
-        while not self.is_ready: time.sleep(.5)
         channel = self.get_channel(CONFIG["channelid"])
-        msg_hst = channel.history(limit=(None if getAll else 10))#.flatten()
+        msg_hst = channel.history(limit=(None if getAll else 50))#.flatten()
         
         # get all messages
-        open("temp/history.json",'w').write(json.dumps([
+        open("data/history.json",'w').write(json.dumps([
             {
                 "message_id":message.id,
                 "author_id":message.author.id,
                 "content":message.content,
                 "date":list(message.created_at.timetuple()),
-             } async for message in msg_hst
+            } async for message in msg_hst
         ], indent=(4 if not getAll else None)))
     
-    async def get_user(self, id):
-       pass 
-        
+    async def get_users(self, users):
+        print("in function users: ", users)
+        dUsers = [await self.fetch_user(user) for user in users]
+        print("in function dUsers",dUsers)
+        open("com/users.json", "w").write(
+            json.dumps([
+                {
+                    user.id : {
+                        "avatar_url":   str(user.avatar_url),
+                        "color":        user.color.value,
+                        "name":         user.display_name,
+                        "discriminator":user.discriminator,
+                    }
+                } for user in dUsers 
+            ], indent=4)
+        )
 
+def get_hst(getAll=False, force=False, update=False):
+    if force and update:
+        try : data = json.loads(open("data/users.json").read())
+        except FileNotFoundError: data = []
+        hst = json.loads(open("data/history.json").read())
+        users = list({message["author_id"] for message in hst})
+        nUsers = [user for user in users if user not in data]
+        print("Users that are not in data : ", nUsers)
+        if not update or not nUsers: return data
+        print("updating the users...")
+        Bot(getMsg=True, getAll=getAll, users=nUsers).run(CONFIG["token"])
+        print("users updated !")
+        open("data/user.json",'w').write(json.dumps(data + json.loads(open("com/users.json",'r').read())))
+        print("users successfuly writed")
+    elif force: Bot(getMsg=True, getAll=getAll).run(CONFIG["token"])
+    elif update:
+        try : data = json.loads(open("data/users.json").read())
+        except FileNotFoundError: data = []
+        hst = json.loads(open("data/history.json").read())
+        users = list({message["author_id"] for message in hst})
+        nUsers = [user for user in users if user not in data]
+        print("Users that are not in data : ", nUsers)
+        if not update or not nUsers: return data
+        print("updating the users...")
+        Bot(users=nUsers).run(CONFIG["token"])
+    return json.loads(open("data/history.json").read())
 
-def get_hst(getAll=False):
-    client = Bot(getMsg=True, getAll=getAll)
-    # messages = await client.get_msgs()
-    client.run(CONFIG["token"])
-    return open("test.json",'r').read()
-
-def get_users(users=CONFIG["testers"]):
-    pass
+def run_bot(test=False):
+    Bot().run(CONFIG["token"])
 
 if __name__ == "__main__":
-    print(get_hst())
+    get_hst(force=True, update=True)
