4cda394800b42bc4520af1fe9e9b4ee58b2a91fb
rboudrouss <r.boudrouss@gmail.com>
2021-02-13 19:23:54 +0100

    didn't work ;v

---
+++ API/discord_utils/helpers.py
@@ -0,0 +1,9 @@
+# not working yet
+import json
+from .bot import *
+
+def get_hst(getAll=False, force=False, update=False):
+    # TODO delete getAll attribute after finalising tests
+    # TODO make this code more clear
+    pass
+
