30561f542516e3622e1698086b08492b7ba28640
rboudrouss <r.boudrouss@gmail.com>
2021-02-16 00:20:48 +0100

    setting default values

--- utils/getData.py
+++ utils/getData.py
@@ -6,13 +6,13 @@ import datetime
 def get_history():
     return json.loads(open("data/history.json").read())
 
-def get_user_msgs(id):
+def get_user_msgs(id=None):
     history = get_history()
     if not id: return history
     user_msgs = [msg for msg in history if msg["author_id"]==int(id)]
     return user_msgs
 
-def get_msgs_date(mind,maxd):
+def get_msgs_date(mind=None,maxd=None):
     """
     max min must respect format : <Y>-<M>-<?D>-<?H>-<?S>-<?MS>
     """
@@ -51,12 +51,12 @@ def get_msgs_date(mind,maxd):
 
     return history[min_index:max_index]
 
-def get_user_date(mind,maxd,id):
+def get_user_date(mind=None,maxd=None,id=None):
     history = get_msgs_date(mind,maxd)
     if not id: return history
     return [msg for msg in history if msg["author_id"]==int(id)]
 
-def get_msg_info(mind,maxd,id,infos):
+def get_msg_info(mind=None,maxd=None,id=None,infos=None):
     history = get_user_date(mind, maxd, id)
     if not infos: return history
     # if type(infos) is list and len(infos)>1: return [{info:msg.get(info, {"Error":"404, Not Found"}) for info in infos} for msg in history]
@@ -67,12 +67,12 @@ def get_msg_info(mind,maxd,id,infos):
 def get_messages(mind,maxd,id,infos): return get_msg_info(mind, maxd, id, infos)
 
 # Users
-def get_users(id): return get_user_data(id)
+def get_users(id=None): return get_user_data(id)
 
 def get_users_data():
     return json.loads(open("data/users.json").read())
 
-def get_user_data(id):
+def get_user_data(id=None):
     users = get_users()
     if not id : return users
     return users.get(str(id),{"Error":"404 User not found"})
