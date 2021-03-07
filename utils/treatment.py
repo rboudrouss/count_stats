5f82461d750df6bbb8f8b9b2f9bb7501abcef098
rboudrouss <r.boudrouss@gmail.com>
2021-03-07 20:19:00 +0100

    changing classement algorithme & update_count cooldown

--- utils/treatment.py
+++ utils/treatment.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-from .getData import get_all_users, get_users_data, get_history
+from .constants import DELAY_COUNT_UPDATE
+from .getData import get_all_users, get_users_data, get_history, get_count
 from .writeData import write_count
 
 
@@ -19,20 +20,15 @@ def create_count():
 
 # TODO create a get_count function and replace create_count
 def create_classement(count_dic=create_count(),top=3):
-    cands = [[0,0]]*top
-    for user,nb in count_dic.items():
-        for i in range(len(cands)):
-            if nb > cands[i][1]:
-                cands[i] = [user,nb]
-    return {
-        f"top{j+1}":cands[j][0] for j in range(len(cands))
-    }
+    return {f"top{i}":k for i,k in enumerate(sorted(count_dic, key=count_dic.get, reverse=True))}
 
 def update_count():
     # TODO update at 5 min interval
-    count = create_count()
-    write_count({
-        "last_update"   : list(datetime.now().timetuple())[:6],
-        "count"         : count,
-        "podium"        : create_classement()
-    })
+    delta = datetime.now()-datetime(*get_count()["last_update"])
+    if delta.seconds>DELAY_COUNT_UPDATE:
+        count = create_count()
+        write_count({
+            "last_update"   : list(datetime.now().timetuple())[:6],
+            "count"         : count,
+            "podium"        : create_classement()
+        })
