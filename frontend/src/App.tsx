3ad0ae0becff83ddc2ded0a085f20f3a81373531
rboudrouss <r.boudrouss@gmail.com>
2021-03-08 01:24:40 +0100

    Cards now kinda works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,27 @@ import React from "react";
 // import './App.css';
 import styles from "./app.module.css";
 
-import { count, users, history, podium, last_update} from "./api/";
+import { getCount, getUsers } from "./api";
 import { Cards, Chart, UserPicker } from "./components";
 
 class App extends React.Component {
   state = {
-    count,
-    users,
-    history,
-    last_update,
-    podium
+    count: {},
+    podium: {},
+    users: {},
+  };
+
+  async componentDidMount() {
+    const { podium, count } = await getCount();
+    const users = await getUsers();
+    this.setState({ podium, count, users });
   }
 
   render() {
-    const {podium, users, last_update} = this.state
+    const { podium, count, users } = this.state;
     return (
       <div className={styles.container}>
-        <Cards podium={podium} users={users} lastUpdate={last_update}/>
+        <Cards podium={podium} count={count} users={users} />
         <Chart />
         <UserPicker />
       </div>
