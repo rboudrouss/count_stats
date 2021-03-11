033dcd4b5ec0ff3ee51796bc919ebf6b72dec2ce
rboudrouss <r.boudrouss@gmail.com>
2021-03-11 21:31:44 +0100

    a working (ugly) chart !

--- frontend/src/components/Chart/Chart.tsx
+++ frontend/src/components/Chart/Chart.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
+import { getMsgInter } from "../../api";
+
 
 const Chart = () => {
-  return <h1>Chart</h1>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setData(await getMsgInter());
+    };
+    console.log(data);
+
+    fetchAPI();
+  });
+
+  const lineChart = data ? (
+    <Line
+      data={{
+        labels: data?.map((item)=>item[0]),
+        datasets: [{
+          data:data?.map((item)=>item[1]),
+          label:"messages",
+          borderColor:"#3333ff",
+          fill:true
+        }],
+      }}
+    />
+  ) : null;
+
+  return (
+    <div className={styles.container}>
+      {lineChart}
+    </div>
+  );
 };
 
 export default Chart;
