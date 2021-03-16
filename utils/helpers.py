7e16cc81dff0a8631aa510d11e9571c1529bede0
rboudrouss <r.boudrouss@gmail.com>
2021-03-16 09:42:00 +0100

    bit of typing and new functions

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,11 +1,31 @@
 from datetime import datetime
+from discord import User, Message
+from .types import UsersData, MessageData, List
 
-def date_from_msg(msg):
+def date_from_msg(msg:List[int])->datetime:
     if msg is list:
         return datetime(*msg)
     else:
         return datetime(*msg["date"])
 
 
-def str_from_date(date:datetime, is_hour=True, is_minute=True):
+def str_from_date(date:datetime, is_hour=True, is_minute=True)->str:
     return f"{date.month}-{date.day}"+(f"-{date.hour}"if is_hour else "") + (f"-{date.minute}" if is_minute else "")
+
+def data_from_message(message:Message)->MessageData:
+    return {
+        "message_id":message.id,
+        "author_id":message.author.id,
+        "content":message.content,
+        "date":list(message.created_at.timetuple())[:6],
+    }
+
+def data_from_user(user:User)->UsersData:
+    return {
+        user.id : {
+            "avatar_url":   str(user.avatar_url),
+            "name":         user.display_name,
+            "discriminator":user.discriminator,
+            "id":           str(user.id),
+        }
+    }
