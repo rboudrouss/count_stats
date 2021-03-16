f35d634c98cc2605b6e5afccb88c666631aef5c2
rboudrouss <r.boudrouss@gmail.com>
2021-03-16 09:33:06 +0100

    adding new components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Cards } from "./Cards/Cards";
 export { default as Chart } from "./Chart/Chart";
 export { default as UserPicker } from "./UserPicker/UserPicker";
+export { default as UserList } from "./UserList/UserList";
+export {default as UserCard} from "./UserCard/UserCard";
