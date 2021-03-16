d231552b6da108c05bac0d333be5e248249a64a1
rboudrouss <r.boudrouss@gmail.com>
2021-03-17 00:33:23 +0100

    fixing a bug after deployement héhé

--- backend/urls.py
+++ backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     re_path(r'^api/', include('API.urls')),
     re_path(r'^admin/', admin.site.urls),
-    re_path(r'.*', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('list',TemplateView.as_view(template_name="index.html")), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
