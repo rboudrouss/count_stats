274dd58401a685d7b45b0fe2f0e572edd103a24d
rboudrouss <r.boudrouss@gmail.com>
2021-03-16 09:39:32 +0100

    making router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,26 @@
 import React from "react";
-// import './App.css';
 import styles from "./app.module.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
+import { MainPage, ListPage, UserPage } from "./pages";
 import { getCount, getUsers } from "./api";
-import { Cards, Chart, UserPicker } from "./components";
 
 class App extends React.Component {
-  state = {
-    count: {},
-    podium: {},
-    users: {},
-    selectedUser: "",
-  };
-  
-  constructor(props: any) {
-    super(props);
-    this.selectedUserChange = this.selectedUserChange.bind(this);
-  }
-
-  async componentDidMount() {
-    const { podium, count } = await getCount();
-    const users = await getUsers();
-    this.setState({ podium, count, users });
-    console.log(users);
-  }
-
-  async selectedUserChange(selectedUser: string) {
-    this.setState({ selectedUser: selectedUser });
-  }
-
   render() {
-    const { podium, count, users, selectedUser } = this.state;
     return (
-      <div className={styles.container}>
-        <Cards podium={podium} count={count} users={users} />
-        <UserPicker
-          users={users}
-          selectedUserChange={this.selectedUserChange}
-        />
-        <Chart selectedUser={selectedUser}/>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/list" component={ListPage} />
+          <Route path="/user" component={UserPage} />
+        </Switch>
+      </Router>
     );
   }
 }
