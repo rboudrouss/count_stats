8b7d6cbf4599a39e9ba06b215b53e0af6d8b50ca
rboudrouss <r.boudrouss@gmail.com>
2021-03-16 17:11:04 +0100

    yeet visual bugs

--- frontend/src/components/UserCard/UserCard.tsx
+++ frontend/src/components/UserCard/UserCard.tsx
@@ -4,23 +4,24 @@ import styles from "./UserCard.module.css";
 import CountUp from "react-countup";
 
 const UserCard = (props: Props) => {
-  console.log(props);
   if (!props.users) {
     return <h1>loading...</h1>;
   }
   var { count, users, user, top, className } = props;
-  console.log(count);
-  console.log(user);
-  console.log(count[user]);
   return (
     <div className={styles.container}>
-      <Grid item component={Card} xs={12} md={4} className={className}>
+      <Grid item component={Card} className={className}>
         <CardContent>
           <Avatar alt={users[user]?.name} src={users[user]?.avatar_url} />
           <Typography variant="h5">{users[user]?.name}</Typography>
           <Typography color="textSecondary">Top {top}</Typography>
           <Typography variant="body2">
-            <CountUp start={0} end={count[user]} duration={3} /> messages !
+            <CountUp
+              start={0}
+              end={count[user] ? count[user] : 0}
+              duration={3}
+            />{" "}
+            messages !
           </Typography>
         </CardContent>
       </Grid>
