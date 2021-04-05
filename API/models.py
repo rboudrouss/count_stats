695256988585fd4093310fc2093ff74e16eadae6
rboudrouss <r.boudrouss@gmail.com>
2021-04-05 21:56:17 +0200

    added MessageData and UserData Models

--- API/models.py
+++ API/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class UserData(models.Model):
+    avatar_url  = models.CharField(max_length=100)
+    name        = models.CharField(max_length=50)
+    discriminator = models.CharField(max_length=5)
+    user_id = models.CharField(max_length=20)
+
+    def __repr__(self):
+        return f"<User {self.name}#{self.discriminator}>"
+
+class MessageData(models.Model):
+    message_id  = models.CharField(max_length=20)
+    author_id   = models.CharField(max_length=20)
+    content     = models.CharField(max_length=2000)
+    date        = models.DateTimeField()
+
+    def __repr__(self):
+        return f"<Message id={self.id} date={date}>"
