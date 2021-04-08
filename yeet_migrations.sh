600cdd78d99945857289fe5d18a823c3c0821c9b
rboudrouss <r.boudrouss@gmail.com>
2021-04-08 10:12:42 +0200

    yeeting automatisation

---
+++ yeet_migrations.sh
@@ -0,0 +1,13 @@
+echo "do you want to yeet the DataBase ? y/N:"
+read conf
+
+if [ $conf = 'y' -o $conf = 'Y' ]; then
+    rm db.sqlite3
+    echo "DATABASE YOOTED !"
+fi
+
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+echo "MIGRATIONS YOOTED ! (making new ones)"
+python manage.py makemigrations && python manage.py migrate
+echo "yeeting out the program..."
