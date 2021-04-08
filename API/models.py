c27a87594ff50e26863a20fe83877d49d55f4049
rboudrouss <r.boudrouss@gmail.com>
2021-04-08 10:13:01 +0200

    changing the models

--- API/models.py
+++ API/models.py
@@ -1,20 +1,27 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
 class UserData(models.Model):
-    avatar_url  = models.CharField(max_length=100)
-    name        = models.CharField(max_length=50)
-    discriminator = models.CharField(max_length=5)
     user_id = models.CharField(max_length=20)
+    avatar_url = models.CharField(max_length=200, default="nan")
+    name = models.CharField(max_length=50, default="nan")
+    discriminator = models.CharField(max_length=5, default="nan")
+    ghost = models.BooleanField(default=True)
+    nb_msg = models.IntegerField(default=0)
+    top = models.IntegerField(default=0)
 
     def __repr__(self):
         return f"<User {self.name}#{self.discriminator}>"
 
+
 class MessageData(models.Model):
-    message_id  = models.CharField(max_length=20)
-    author_id   = models.CharField(max_length=20)
-    content     = models.CharField(max_length=2000)
-    date        = models.DateTimeField()
+    message_id = models.CharField(max_length=20)
+    author_id = models.CharField(max_length=20, default="0")
+    content = models.CharField(max_length=2000, default="nan")
+    date = models.DateTimeField(default=datetime.now())
 
     def __repr__(self):
-        return f"<Message id={self.id} date={date}>"
+        return f"<Message id={self.message_id} date={self.date}>"
