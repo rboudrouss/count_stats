bf9939e03b6f25ab0a02b8996243b1775e2ff3c0
rboudrouss <r.boudrouss@gmail.com>
2021-04-08 10:56:37 +0200

    correcting typing

--- utils/types.py
+++ utils/types.py
@@ -1,31 +1,38 @@
-from typing import List, TypedDict, Dict, Annotated, Union, Optional
+from typing import List, TypedDict, Dict, Union
 
 # MaxLen : 6
 DateList = List[int]
 
+# TODO FALSE, correct this
+
+
 class UserData(TypedDict):
+    discriminator: str
+    user_id: str
+    name: str
     avatar_url: str
-    author_id : int
-    content   : str
-    date      : DateList
+
 
 class MessageData(TypedDict):
-    message_id: int
-    author_id : int
-    content   : str
-    date      : DateList
+    message_id: str
+    author_id: str
+    content: str
+    date: DateList
 
-UsersData = Dict[int, UserData]
 
-UserId = Union[str,int]
+UsersData = List[UserData]
 
-Count = Dict[str,int]
+UserId = Union[str, int]
 
-Podium = Dict[str,str]
+Count = Dict[str, int]
+
+Podium = Dict[str, str]
+
 
 class AllCount(TypedDict):
-    last_update:DateList
-    count:Count
+    last_update: DateList
+    count: Count
     podium: Podium
 
-MsgCount = List[Union[str,int]]
+
+MsgCount = List[Union[str, int]]
