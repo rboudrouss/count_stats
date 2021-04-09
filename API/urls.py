1b177a9dc0300629a9e027b714054c77d21394f7
rboudrouss <r.boudrouss@gmail.com>
2021-04-09 22:08:24 +0200

    don't need those

--- API/urls.py
+++ API/urls.py
@@ -8,15 +8,13 @@ urlpatterns = [
     path('message/history', views.get_history, name='history'),
     path('message/usermsg', views.user_msgs, name='user messages'),
     path('message/datemsg', views.date_msgs, name='date messages'),
-    path('message/userdate', views.user_date, name='user date message'),
+    # path('message/userdate', views.user_date, name='user date message'),
     path('message/inter', views.inter_msg, name="nb message by interval"),
     path('message', views.messages, name="Messages"),
     # user data
     path('user/users', views.users_data, name='users'),
     path('user/user', views.user_data, name='user'),
     path('user', views.users, name="user"),
-    # count
-    path('count', views.count, name="count"),
     # write
     path('write/history', views.write_history, name='write history'),
     path('write/users', views.write_users, name="write users"),
