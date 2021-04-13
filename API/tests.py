75626d8ebd8bd3d5fecc126ae52770cbcdbbd62c
rboudrouss <r.boudrouss@gmail.com>
2021-04-13 22:37:12 +0200

    adding some tests

--- API/tests.py
+++ API/tests.py
@@ -1,8 +1,33 @@
 from django.test import TestCase
 
+from .models import UserData, MessageData
+
 # Create your tests here.
 
 
-class APITesting(TestCase):
+class ModelTesting(TestCase):
     def setUp(self):
-        pass
+        self.user = UserData.objects.create(
+            user_id="1",
+            avatar_url="https://rboud.ml/img/rboud-01.png",
+            name="rboud",
+            discriminator="1207",
+            ghost=False,
+            nb_msg=1,
+            top=1
+        )
+        self.message = MessageData.objects.create(
+            message_id="1",
+            author_id="1",
+            content="fromage",
+            date="2021-04-13T21:55:08.532862"
+        )
+
+    def test_post_model(self):
+        self.assertTrue(isinstance(self.user, UserData))
+        self.assertTrue(isinstance(self.message, MessageData))
+
+    def test_repr_model(self):
+        self.assertEqual(repr(self.user), "<User rboud#1207>")
+        self.assertEqual(repr(self.message),
+                         "<Message id=1 date=2021-04-13T21:55:08.532862>")
