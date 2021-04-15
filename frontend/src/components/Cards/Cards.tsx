4facc411d69413d9aa5026865bdedcdc87c783f1
rboudrouss <r.boudrouss@gmail.com>
2021-04-15 03:07:55 +0200

    forgot to type that

--- frontend/src/components/Cards/Cards.tsx
+++ frontend/src/components/Cards/Cards.tsx
@@ -3,28 +3,29 @@ import { Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 import cx from "classnames";
 import { UserCard } from "..";
+import { User } from "../../types"
 
-const Cards = (props: any) => {
+const Cards = (props: { users: User[] }) => {
   if (!props.users) {
     return <h1>loading...</h1>;
   }
   var {
-    users,
+    users
   } = props;
   return (
     <div className={styles.container}>
-      <Grid container spacing={3} justify="center">
+      <Grid container spacing={3} justify="space-evenly">
         <UserCard
           user={users[0]}
           className={cx(styles.card, styles.top1)}
         />
         <UserCard
-          user={users[1]}
-          className={cx(styles.card, styles.top2)}
-        />
-        <UserCard
           user={users[2]}
           className={cx(styles.card, styles.top3)}
+        />
+        <UserCard
+          user={users[1]}
+          className={cx(styles.card, styles.top2)}
         />
       </Grid>
     </div>
