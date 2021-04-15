a930421b1d8d1faa07b30dbc24cfee95a57c1b9d
rboudrouss <r.boudrouss@gmail.com>
2021-04-15 03:06:58 +0200

    adding intro & theme

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { ThemeProvider } from '@material-ui/styles';
+import { Typography, Grid } from "@material-ui/core";
 import styles from "./MainPage.module.css";
 
 import { getUsers } from "../../api";
-import { Cards, Chart, UserPicker } from "../../components";
+import { Cards, Chart, UserPicker, TopBar } from "../../components";
 import { User } from "../../types"
+import theme from "../../theme"
 
 class MainPage extends React.Component {
   state: { users: User[], selectedUser: string } = {
@@ -28,14 +31,31 @@ class MainPage extends React.Component {
   render() {
     const { users, selectedUser } = this.state;
     return (
-      <div className={styles.container}>
-        <Cards users={users} />
-        <UserPicker
-          users={users}
-          selectedUserChange={this.selectedUserChange}
-        />
-        <Chart selectedUser={selectedUser} />
-      </div>
+      <ThemeProvider theme={theme}>
+        <TopBar />
+        <div className={styles.container}>
+          <section className={styles.intro}>
+            <Typography variant="h4" color="secondary">
+              Bienvenue sur count stats !
+            </Typography>
+            <Typography color="textSecondary">
+              count_stats (toujours à la recherche d'un meilleur nom) est un site relevant les statistiques d'activité d'un
+              channel discord.
+            </Typography>
+          </section>
+          <section className={styles.cards}>
+            <Typography variant="h5" color="textSecondary">
+              Podium
+            </Typography>
+            <Cards users={users} />
+          </section>
+          <UserPicker
+            users={users}
+            selectedUserChange={this.selectedUserChange}
+          />
+          <Chart selectedUser={selectedUser} />
+        </div>
+      </ThemeProvider>
     );
   }
 }
---
+++ frontend/src/theme.ts
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: {
+            main: "#103547",
+        },
+        secondary: {
+            main: "#837a72",
+        },
+    },
+});
+
+export default theme;
