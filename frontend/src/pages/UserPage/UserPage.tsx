6d2f9143a6a3754d50a1820fc96dd26d0f374d8b
rboudrouss <r.boudrouss@gmail.com>
2021-04-15 23:17:26 +0200

    new page

--- frontend/src/pages/UserPage/UserPage.tsx
+++ frontend/src/pages/UserPage/UserPage.tsx
@@ -1,15 +1,39 @@
 
 import React from "react";
-
 import styles from "./UserPage.module.css";
+import { TopBar } from "../../components"
+import { User } from "../../types"
+import { getUsers } from "../../api";
 
 class UserPage extends React.Component {
+  state: { id: string, users: User[] } = {
+    id: (this.props as any).match.params.id as string, // HACK
+    users: []
+  }
+
+  async componentDidMount() {
+    const users = await getUsers();
+    this.setState({ users });
+  }
+
   constructor(props: {}) {
     super(props);
   }
 
+
   render() {
-    return <h1>UsersPage</h1>;
+    const { users, id } = this.state;
+    var exits = false;
+    for (const user of users) {
+      if (user.user_id === id) {
+        exits = true;
+      }
+    }
+    return (
+      <div className={styles.container}>
+        <TopBar />
+      </div>
+    );
   }
 }
 
